bb7e0b16c00a4695cc41e81b0c883d918673ec73
Alona <alonatunina@gmail.com>
2024-07-22 23:24:00 +0300

    add js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
-function makeTransaction (quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-};
+const arrayOfLi = document.querySelectorAll(".item");
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(`Numbers of categories: ${arrayOfLi.length}`);
+
+
+arrayOfLi.forEach(array => {
+  const categoryName = array.querySelector('h2').textContent;
+  console.log(`Category: ${categoryName}`)
+  const liCount =array.querySelectorAll('ul li').length
+  console.log(liCount);
+})
+
+
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
-function getElementWidth (content, padding, border){
-  return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-};
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+input.addEventListener("input", setOutput);
+
+function setOutput(event) {
+  const nameValue = event.target.value;
+  output.textContent = nameValue.trim() ?  nameValue : "Anonymous"
+}
